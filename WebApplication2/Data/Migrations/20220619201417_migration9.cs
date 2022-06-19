5bf6b399d8480deac3bd263b2b06a5b8ffbad368
JakubLeszcz <JakubLeszcz@users.noreply.github.com>
2022-06-19 23:21:08 +0200

    final version

---
+++ WebApplication2/Data/Migrations/20220619201417_migration9.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication2.Data.Migrations
+{
+    public partial class migration9 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SuccesfulBets",
+                table: "Player",
+                newName: "SuccessfulBets");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SuccessfulBets",
+                table: "Player",
+                newName: "SuccesfulBets");
+        }
+    }
+}
